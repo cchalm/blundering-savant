93805672903f5af65a37d12352550a5cccac4ba7
Chris Chalmers <chrischalmers.cpc@gmail.com>
2025-09-02 23:34:53 -0700

    Fix always loading env vars for all commands

--- app/blundering-savant/cmd/root.go
+++ app/blundering-savant/cmd/root.go
@@ -13,15 +13,14 @@ var rootCmd = &cobra.Command{
 	Long: `Blundering Savant is a generative AI coding agent that presents as a GitHub user.
 The agent receives instructions via issues, reviews, and comments and proposes code 
 changes by creating and updating pull requests.`,
+	PreRun: loadRootConfig,
 }
 
 func Execute() error {
 	return rootCmd.Execute()
 }
 
-func init() {
-	config = Config{}
-
+func loadRootConfig(_ *cobra.Command, _ []string) {
 	// Load .env file
 	err := godotenv.Load()
 	if err != nil {
@@ -31,6 +30,9 @@ func init() {
 	loadFromEnv(&config.SystemGithubToken, "SYSTEM_GITHUB_TOKEN")
 	loadFromEnv(&config.BotGithubToken, "BOT_GITHUB_TOKEN")
 	loadFromEnv(&config.AnthropicAPIKey, "ANTHROPIC_API_KEY")
+}
 
+func init() {
 	rootCmd.PersistentFlags().StringVar(&config.ValidationWorkflowName, "validation-workflow", "", "GitHub Actions workflow name for validation")
+	_ = oneShotCmd.MarkFlagRequired("validation-workflow")
 }
