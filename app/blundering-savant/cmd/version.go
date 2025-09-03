aa8085c4b7ebaa43b5c57d9c3d662eb2d52a762c
Blundering Savant <chrischalmers.cpc+blunderingsavant@gmail.com>
2025-09-03 08:53:31 -0700

    Hook up version command to build-time variables
    - Add Version, GitCommit, and BuildTime variables to main.go that can be set via ldflags
    - Create SetVersionInfo function in cmd package to pass version info from main
    - Update version command to display actual version, git commit, and build time instead of hardcoded string
    - Maintains backward compatibility with default values when variables aren't set via ldflags
    
    Fixes #71

--- app/blundering-savant/cmd/version.go
+++ app/blundering-savant/cmd/version.go
@@ -6,11 +6,27 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// Version information set by main package
+var (
+	version   = "dev"
+	gitCommit = "unknown"
+	buildTime = "unknown"
+)
+
+// SetVersionInfo sets the version information from build-time variables
+func SetVersionInfo(v, commit, buildT string) {
+	version = v
+	gitCommit = commit
+	buildTime = buildT
+}
+
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print version information",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Blundering Savant Bot v1.0.0") // TODO version number
+		fmt.Printf("Blundering Savant Bot %s\n", version)
+		fmt.Printf("Git Commit: %s\n", gitCommit)
+		fmt.Printf("Build Time: %s\n", buildTime)
 	},
 }
 
